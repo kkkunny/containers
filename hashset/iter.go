3874fc7bc7686ee020c352bed6a1b25853de7cab
kkkunny <51853085+kkkunny@users.noreply.github.com>
2023-04-05 02:23:03 +0000

    add iter for hashset

---
+++ hashset/iter.go
@@ -0,0 +1,35 @@
+package hashset
+
+import "github.com/kkkunny/containers/hashmap"
+
+type HashSetIter[T comparable] struct {
+	iter *hashmap.HashMapIter[T, struct{}]
+}
+
+func (hs *HashSet[V]) Iterator() *HashSetIter[V] {
+	if hs.Length() == 0 {
+		return nil
+	}
+	return &HashSetIter[V]{iter: hs.data.Iterator()}
+}
+
+func (i *HashSetIter[V]) Value() V {
+	return i.iter.Key()
+}
+func (i *HashSetIter[V]) HasNext() bool {
+	return i.iter.HasNext()
+}
+func (i *HashSetIter[V]) Next() {
+	i.iter.Next()
+}
+
+func HashSetMap[FV comparable, TV comparable](hs *HashSet[FV], fn func(v FV) TV) *HashSet[TV] {
+	newHs := NewHashSetWith[TV](hs.Length())
+	for iter := hs.Iterator(); iter != nil; iter.Next() {
+		newHs.Add(fn(iter.Value()))
+		if !iter.HasNext() {
+			break
+		}
+	}
+	return newHs
+}
--- hashset/stringer.go
+++ hashset/stringer.go
@@ -8,11 +8,11 @@ import (
 func (hs *HashSet[T]) String() string {
 	var buf strings.Builder
 	buf.WriteByte('{')
-	for iter:=hs.data.Iterator(); iter!=nil; iter.Next() {
+	for iter := hs.data.Iterator(); iter != nil; iter.Next() {
 		buf.WriteString(fmt.Sprintf("%v", iter.Key()))
 		if iter.HasNext() {
 			buf.WriteString(", ")
-		}else{
+		} else {
 			break
 		}
 	}
